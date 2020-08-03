97b189271db90450b8fc41b8a0a50bfa97a2d852
Ewilan <ewilan@dotslashplay.it>
2020-08-03 17:33:28 +0200

    ifix

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,14 +3,14 @@ import VueDependenciesBadges from 'vue-dependencies-badges'
 import Translation from './config/locales/index'
 import './plugins/vue-icons-loader'
 import './plugins/global-components-loader'
-import VueCodeBlock from 'vue-code-block'
+import VuepressCodeInfo from 'vuepress-code-info'
 
 export default ({ Vue, isServer, options, router }) => {
   // Vue.prototype.$axios = axios
 
   Vue.use(VueDependenciesBadges)
   Vue.use(Translation)
-  Vue.use(VueCodeBlock)
+  Vue.use(VuepressCodeInfo)
   // if (!isServer) {
   //   import('vue-toasted' /* webpackChunkName: "notification" */).then(
   //     (module) => {
