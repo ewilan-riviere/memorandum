de5cc9b77fc7b30afa71d9a0591f3d237c329f69
Ewilan <ewilan@dotslashplay.it>
2020-11-05 11:56:49 +0100

    fix prod

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 export default {
-  target: 'static',
+  // target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Memorandum',
