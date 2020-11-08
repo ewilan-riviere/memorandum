5dc0cac5932fab7a48b107fff4d472e76965a7ef
Ewilan Rivière <ewilan.riviere@gmail.com>
2020-11-08 14:37:31 +0100

    add new md img component

--- components/global/markdown/MdImg.vue
+++ components/global/markdown/MdImg.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex">
     <img
-      :src="`/images/posts/${$route.params.slug}/${source}`"
+      :src="`/documentation/${path}/${source}`"
       :alt="source"
-      class="mx-auto shadow-md"
+      class="mx-auto rounded-md shadow-md"
     />
   </div>
 </template>
@@ -15,6 +15,23 @@ export default {
     source: {
       type: String,
       default: null,
+    },
+  },
+  data() {
+    return {
+      path: null,
+    }
+  },
+  mounted() {
+    this.getPath()
+  },
+  methods: {
+    getPath() {
+      let path = this.$route.fullPath.split('/')
+      path.splice(-1, 1)
+      path.splice(0, 1)
+      path = path.join('/')
+      this.path = path
     },
   },
 }
