099a74995285cd597bfc52c8f8fdf91227c110d3
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-03-12 12:55:47 +0100

    🔥 clean

--- archive/plugins/vue-badges.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import VueBadges from 'vue-badges'
-
-Vue.use(VueBadges)
--- archive/plugins/vue-lazy-youtube.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import LazyYoutubeVideo from 'vue-lazy-youtube-video'
-
-Vue.component('LazyYoutubeVideo', LazyYoutubeVideo)
