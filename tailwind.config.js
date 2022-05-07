fcf6f565ae2ce2a7458601bfed20daba9b45a3ad
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-05-07 18:38:43 +0200

    config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+    require('tailwindcss-debug-screens'),
+    require('tailwind-scrollbar'),
+  ],
+}
