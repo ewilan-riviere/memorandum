37eebd83f8c0bb1cbe19535dd02f799a800a036d
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-07-01 09:21:57 +0200

    fix

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -3,16 +3,28 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible"
+          content="IE=edge">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token"
+          content="{{ csrf_token() }}">
 
     {!! SEO::generate() !!}
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
+    <link rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png">
+    <link rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png">
+    <link rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png">
+    <link rel="manifest"
+          href="/site.webmanifest">
 
     @vite()
 
@@ -21,7 +33,7 @@
 </head>
 
 <body x-data
-    class="bg-gray-900 text-white overflow-hidden {{ config('app.env') === 'local' ? 'debug-screens' : '' }}">
+      class="{{ config('app.env') === 'local' ? 'debug-screens' : '' }} overflow-hidden bg-gray-900 text-white">
     <x-toast />
     <div class="h-screen">
         {{ $slot }}
