11213fda9895b29d3cc895fa4de402c5a4ba1466
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-01-20 12:46:19 +0100

    add php syntax

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
 export default defineNuxtConfig({
+  content: {
+    highlight: {
+      preload: ['diff', 'json', 'js', 'ts', 'css', 'shell', 'html', 'md', 'yaml', 'php']
+    }
+  },
   extends: '@nuxt-themes/docus'
 })
